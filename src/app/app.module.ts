c6e35b8f1ac0e4ef6b6c2fcb439bac5d010ec91b
Marcos Gabriel <jorgede1097@gmail.com>
2021-12-13 22:28:36 -0300

    Layout base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/shared/nav/nav.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { MainComponent } from './components/shared/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
